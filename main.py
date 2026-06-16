67ce5058aa5a760105c4e2698d512acced4d9dad
Yasin Akkus <akkusoft@gmail.com>
2026-06-17 00:10:17 +0200

    Create Store class inside store.py and tested it within main.py

--- main.py
+++ main.py
@@ -2,17 +2,14 @@
 This is the main file within this repository.
 In here everything else needed will be imported and consumed.
 """
-from products import Product
+from store import Store
+import products
+product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                products.Product("Google Pixel 7", price=500, quantity=250),
+               ]
 
-bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-mac = Product("MacBook Air M2", price=1450, quantity=100)
-
-print(bose.buy(50))
-print(mac.buy(100))
-print(mac.is_active())
-
-bose.show()
-mac.show()
-
-bose.set_quantity(1000)
-bose.show()
+best_buy = Store(product_list)
+products = best_buy.get_all_products()
+print(best_buy.get_total_quantity())
+print(best_buy.order([(products[0], 1), (products[1], 2)]))
